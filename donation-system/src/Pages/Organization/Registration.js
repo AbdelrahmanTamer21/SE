827e1a5e3f5b350eefbc80297e90cc0c141b5e02
anastamerr <anastamer136@gmail.com>
2024-04-28 23:11:52 +0300

    Updated reg form

--- donation-system/src/Pages/Organization/Registration.js
+++ donation-system/src/Pages/Organization/Registration.js
@@ -1,49 +1,47 @@
-// ./Pages/Organization/Registration.js
-import React, { useState } from 'react';
+import React from 'react';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import { PiWarehouse } from "react-icons/pi";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { MdOutlineMail } from "react-icons/md";
+import { FaKey } from "react-icons/fa";
 
-function Registration() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phoneNumber: ''
-  });
-  const [documents, setDocuments] = useState([]);
-
-  const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleFileChange = (e) => {
-    const files = Array.from(e.target.files);
-    setDocuments(files);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form data:', formData);
-    console.log('Uploaded documents:', documents);
-  };
-
+function App() {
   return (
-    <div>
-      <h2>Organization Registration Form</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Name:</label>
-        <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
-
-        <label>Email:</label>
-        <input type="email" name="email" value={formData.email} onChange={handleInputChange} />
-
-        <label>Phone Number:</label>
-        <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} />
-
-        <label>Upload Documents:</label>
-        <input type="file" multiple onChange={handleFileChange} />
-
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <Container fluid>
+      <Card className='text-black m-5' style={{ borderRadius: '25px' }}>
+        <Card.Body>
+          <Row>
+            <Col md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
+              <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Register your Organization</p>
+              <div className="d-flex flex-row align-items-center mb-4 ">
+                <PiWarehouse className="me-3" size='24'/>
+                <Form.Control type='text' placeholder='Organization Name' className='w-100' />
+              </div>
+              <div className="d-flex flex-row align-items-center mb-4">
+                <MdOutlineMail className="me-3" size='24'/>
+                <Form.Control type='email' placeholder='Organization Email' />
+              </div>
+              <div className="d-flex flex-row align-items-center mb-4">
+                <RiLockPasswordLine className="me-3" size='24'/>
+                <Form.Control type='password' placeholder='Password' />
+              </div>
+              <div className="d-flex flex-row align-items-center mb-4">
+                <FaKey className="me-3" size='24'/>
+                <Form.Control type='password' placeholder='Repeat password' />
+              </div>
+              <div className='mb-4'>
+                <Form.Check type='checkbox' id='flexCheckDefault' label='Subscribe to our newsletter' />
+              </div>
+              <Button className='mb-4' size='lg'>Register</Button>
+            </Col>
+            <Col md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
+              <Card.Img src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid />
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
 
-export default Registration;
+export default App;
