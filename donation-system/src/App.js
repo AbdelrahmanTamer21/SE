ba039399b8fe052a1fb50540936c303f41eafad0
Abdelrahman Tamer <abdelrahmantamer2005@yahoo.com>
2024-04-29 19:02:25 +0300

    Fixed styling

--- donation-system/src/App.js
+++ donation-system/src/App.js
@@ -1,11 +1,8 @@
-import Button from 'react-bootstrap/esm/Button';
-import './App.css';
-import TopBar from './Components/TopBar';
+import './App.css';import TopBar from './Components/TopBar';
 import { UserProvider } from './Components/UserContext';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import Home from './Pages/Home';
-import React from 'react';
 
 function App() {
   return (
