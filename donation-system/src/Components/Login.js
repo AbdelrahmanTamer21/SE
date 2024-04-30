5323ebb377cc5e951feb452479d9c89d6fbe0270
omarmosallam <omar.m.mohamed.om@gmail.com>
2024-04-30 15:10:31 +0300

    setup Login Page

--- donation-system/src/Components/Login.js
+++ donation-system/src/Components/Login.js
@@ -1,12 +1,21 @@
 import { useNavigate } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form'; // Add this line
 
 function Login(){
     const navigate = useNavigate();
     return (
         <div>
-            Login
+            <h1>Login</h1>
+            <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                <Form.Control
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                />
+            </InputGroup>
         </div>
     );
 }
