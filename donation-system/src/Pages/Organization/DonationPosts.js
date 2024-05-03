ce29486645d0d9aa6019367328631872b059c119
anastamerr <anastamer136@gmail.com>
2024-05-03 16:57:59 +0300

    Minor error

--- donation-system/src/Pages/Organization/DonationPosts.js
+++ donation-system/src/Pages/Organization/DonationPosts.js
@@ -58,7 +58,56 @@ function DonationForm() {
           <Card>
             <Card.Body className='px-4'>
               <Form onSubmit={handleSubmit}>
-                {/* Form fields... */}
+                <Row className='align-items-center pt-4 pb-3'>
+                  <Col md='3' className='ps-5'>
+                    <h5 className="mb-0">Category</h5>
+                  </Col>
+                  <Col md='9' className='pe-5'>
+                    <Form.Select size='lg' name='category' value={formData.category} onChange={handleInputChange}>
+                      <option value="">Select Category</option>
+                      <option value="Clothing">Clothing</option>
+                      <option value="Food">Food</option>
+                      <option value="Toys">Toys</option>
+                      <option value="Other">Other</option> {/* Added "Other" option */}
+                    </Form.Select>
+                  </Col>
+                </Row>
+                {/* Other form fields... */}
+                <Row className='align-items-center pt-4 pb-3'>
+                  <Col md='3' className='ps-5'>
+                    <h5 className="mb-0">Item Name</h5>
+                  </Col>
+                  <Col md='9' className='pe-5'>
+                    <Form.Control type='text' placeholder='Item Name' name='itemName' value={formData.itemName} onChange={handleInputChange} size='lg' />
+                  </Col>
+                </Row>
+                <hr className="mx-n3" />
+                <Row className='align-items-center pt-4 pb-3'>
+                  <Col md='3' className='ps-5'>
+                    <h5 className="mb-0">Condition</h5>
+                  </Col>
+                  <Col md='9' className='pe-5'>
+                    <Form.Select size='lg' name='condition' value={formData.condition} onChange={handleInputChange}>
+                      <option value="">Select Condition</option>
+                      <option value="New">New</option>
+                      <option value="Other">Used</option> {/* Added "Other" option */}
+                    </Form.Select>
+                    {/* Conditional rendering for "Other" option */}
+                    {formData.condition === 'Other' && (
+                      <Form.Control type='text' placeholder='Other Condition' name='conditionOther' value={formData.conditionOther} onChange={handleInputChange} size='lg' />
+                    )}
+                  </Col>
+                </Row>
+                <hr className="mx-n3" />
+                <Row className='align-items-center pt-4 pb-3'>
+                  <Col md='3' className='ps-5'>
+                    <h5 className="mb-0">Description</h5>
+                  </Col>
+                  <Col md='9' className='pe-5'>
+                    <Form.Control as='textarea' placeholder='Description' name='description' value={formData.description} onChange={handleInputChange} rows={3} />
+                  </Col>
+                </Row>
+                <hr className="mx-n3" />
                 <Row className='justify-content-center align-items-center'>
                   <Col md='6' className='text-center'>
                     <Button className='my-4 mx-2' size='lg' type='submit'>Submit</Button>
