022f2189cb02ad0ad84d8f4d9f72604872ba5628
Seifo18 <sksoliman18@gmail.com>
2024-05-07 15:28:57 +0300

    added organizations to sidebar of donor

--- donation-system/src/Components/SideBar.js
+++ donation-system/src/Components/SideBar.js
@@ -36,7 +36,7 @@ const Sidebar = () => {
             <Nav.Link eventKey="link-1" onClick={()=>navigate(`/${userRole}/VolunteerSelection`)} className="nav-link">Volunteer Role Selection</Nav.Link>
             <Nav.Link eventKey="link-1" onClick={()=>navigate(`/${userRole}/DocumentUpload`)} className="nav-link">Document Upload</Nav.Link>
             <Nav.Link eventKey="link-1" onClick={()=>navigate(`/${userRole}/Delivery`)} className="nav-link">Delivery Details</Nav.Link>
-
+            <Nav.Link eventKey="link-1" onClick={()=>navigate(`/${userRole}/Organizations`)} className="nav-link">Organizations</Nav.Link>
           </>
         ):null}
         <Nav.Link eventKey="link-3" className="nav-link">Profile</Nav.Link>
