174af9d029a28adaa119dd353c809d9a5f5ee26d
hassankhaledxx <hk5344349@gmail.com>
2024-05-09 19:12:06 +0300

    medicalcases Table bs lesa 3ayz ytzbt w mehtag ytzbt feeh filtering

---
+++ donation-system/src/medicalCasesData.js
@@ -0,0 +1,31 @@
+let medicalCasesData = [
+    {
+      key: 1,
+      id: 1,
+      patientname:"Kareem Rafaat",
+      patientage:"19",
+      patientgender:"Male",
+      patientweight:"63",
+      patientlocation:"",
+      address:"Tagamoaa",
+      OrgName:"57357",
+      medicalSpeciality:"Blood Cancer",
+      caseDesc:"Has high level of blood cancer"
+    },
+    {
+      key: 2,
+      id: 2,
+      patientname:"Kareem Sayed",
+      patientage:"12",
+      patientgender:"female",
+      patientweight:"45",
+      patientlocation:"",
+      address:"Tagamoaa",
+      OrgName:"57357",
+      medicalSpeciality:"Blood Cancer",
+      caseDesc:"Has high level of blood cancer"
+    },
+  
+  ];
+  
+  export default medicalCasesData;
