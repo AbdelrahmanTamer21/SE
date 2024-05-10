c4d030955aa545fb6b1263481b2ee5d0c11ac034
hassankhaledxx <hk5344349@gmail.com>
2024-05-11 02:20:17 +0300

    adjust avater button and added view document for donor

--- donation-system/src/Components/Avatar.js
+++ donation-system/src/Components/Avatar.js
@@ -22,14 +22,14 @@ function Avatar() {
 
     return (
         <Dropdown>
-            <Dropdown.Toggle variant="secondary" style={{ backgroundColor: '#D3D3D3' }} id="avatar-dropdown" className="">
+            <Dropdown.Toggle onMouseEnter={e => e.target.style.backgroundColor='grey'} onMouseLeave={e => e.target.style.backgroundColor='#D3D3D3'}  variant="secondary" style={{ backgroundColor: '#D3D3D3' }} id="avatar-dropdown" className="">
                 <Image src="https://i.pinimg.com/564x/c0/27/be/c027bec07c2dc08b9df60921dfd539bd.jpg" roundedCircle width="30" height="30" />
                 <i className="dropdown-icon fas fa-caret-down"></i>
             </Dropdown.Toggle>
             <Dropdown.Menu>
                 <Dropdown.Item as={Link} to={`/${userRole}/Profile`}>Profile</Dropdown.Item>
                 <Dropdown.Item as={Link} to={`/${userRole}/Settings`}>Settings</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Notifications</Dropdown.Item>
                 <Dropdown.Divider />
                 <Dropdown.Item>
                     <Button variant="danger" onClick={handleLogout} className=" w-100" >Logout</Button>
--- donation-system/src/Pages/Admin/Requests.js
+++ donation-system/src/Pages/Admin/Requests.js
@@ -45,6 +45,12 @@ export function DonorsTab() {
             }
         });
     }
+    const handleViewDocument = (donor) => {
+        // Add logic to view the organization's document
+        const pdf = new Blob([donor.pdf], { type: 'application/pdf' });
+        const url = URL.createObjectURL(pdf);
+        window.open(url);
+    }
 
     return (
         <div>
@@ -71,6 +77,7 @@ export function DonorsTab() {
                                     <h2>{donor.first_name + " " + donor.last_name}</h2>
                                     <p>{donor.donorEmail}</p>
                                     <p>{donor.don_Type}</p>
+                                    <Button variant='main-inverse' className="btn-primary  w-100" onClick={() => handleViewDocument(donor)}>View Document</Button>
                                     <Button className="btn-success m-3" style={{ borderRadius: '50%', width: '3rem', height: '3rem' }} onClick={() => handleAccept(donor)}>
                                         <BiCheck size={25} />
                                     </Button>
@@ -150,7 +157,7 @@ export function OrganizationsTab() {
                                 <h2>{org.organizationName}</h2>
                                 <p>{org.organizationEmail}</p>
                                 <p>{org.orgType}</p>
-                                <Button className="btn-primary  w-100" onClick={() => handleViewDocument(org)}>View Document</Button>
+                                <Button variant='main-inverse' className="btn-primary  w-100" onClick={() => handleViewDocument(org)}>View Document</Button>
                                 <Button className="btn-success m-3" style={{ borderRadius: '50%', width: '3rem', height: '3rem' }} onClick={() => handleAccept(org)}>
                                     <BiCheck size={25} />
                                 </Button>
