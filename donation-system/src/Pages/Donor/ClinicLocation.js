29127850c897dab4d6eaa540908c0c7174f0d9d7
Seifo18 <sksoliman18@gmail.com>
2024-05-11 18:11:05 +0300

    modified clinic location and subject

--- donation-system/src/Pages/Donor/ClinicLocation.js
+++ donation-system/src/Pages/Donor/ClinicLocation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 import { FaHome } from "react-icons/fa";
 import { FaCity } from "react-icons/fa";
@@ -9,10 +9,37 @@ import { MdEventAvailable } from "react-icons/md";
 import Doctor from '../Doctor.png';
 import LoginData from '../LoginData';
 import { useNavigate, useParams } from 'react-router-dom';
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import markerIcon from '../marker.png'; // Import your marker icon
+import L from 'leaflet';
 
 function ClinicLocation() {
   const navigate = useNavigate();
   const { username } = useParams();
+
+  const [markerPosition, setMarkerPosition] = useState(null);
+  const mapRef = useRef(null);
+
+  // Create custom marker icon
+  const customMarkerIcon = L.icon({
+    iconUrl: markerIcon,
+    iconSize: [ 22, 32 ], // Adjust the size of your marker icon
+  });
+
+  function handleMapClick(e) {
+    setMarkerPosition(e.latlng);
+  }
+
+  function handleSetLocation() {
+    const map = mapRef.current;
+    if (map) {
+      const center = map.getCenter();
+      console.log(center);
+      setMarkerPosition(center);
+    }
+  }
+
   function handleSubmit() {
     LoginData.forEach((user) => user.username === username ? user.type = 'Doctor' : null)
     navigate("/Login")
@@ -38,10 +65,6 @@ function ClinicLocation() {
                   <Form.Control type='text' placeholder='Clinic Government' required />
                 </div>
                 <div className="d-flex flex-row align-items-center mb-4">
-                  <CiLocationOn className="me-3" size='24' />
-                  <Form.Control type='text' placeholder='Google Marker' required />
-                </div>
-                <div className="d-flex flex-row align-items-center mb-4">
                   <FaUserDoctor className="me-3" size='24' />
                   <Form.Control type='text' placeholder='Speciality' required />
                 </div>
@@ -49,15 +72,34 @@ function ClinicLocation() {
                   <MdEventAvailable className="me-3" size='24' />
                   <Form.Control type='number' placeholder='Pro-bono cases' required />
                 </div>
-
-
-
+                <Form.Group className="mb-4">
+                    <Form.Label>Document Upload for Organization Verification</Form.Label>
+                    <Form.Control type="file" name='document'  required />
+                  </Form.Group>
                 <Button variant='main-inverse' type='submit' className='mb-4' size='lg'>Submit</Button>
               </Form>
             </Col>
             <Col md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
-              <Card.Img width={100} height={450} variant="top" src={Doctor} />
-            </Col>
+                <div style={{ border: '2px solid black', width: '100%', height: '400px', position: 'relative' }}>
+                  <MapContainer center={[30.020882, 31.526789]} zoom={13} style={{ width: '100%', height: '100%' }} onclick={handleMapClick} ref={mapRef}>
+
+                    <TileLayer
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    {markerPosition && (
+                      <Marker position={markerPosition} icon={customMarkerIcon}>
+                        <Popup>
+                          Your location
+                        </Popup>
+                      </Marker>
+                    )}
+                  </MapContainer>
+                  <Row className='justify-content-center' style={{ position: 'absolute', bottom: '-70px', left: '50%', transform: 'translateX(-50%)' }}>
+                    <Button variant='main-inverse' onClick={handleSetLocation} ><FaMapMarkerAlt />  Set Location of your Clinic </Button>
+                  </Row>
+                </div>
+              </Col>
           </Row>
         </Card.Body>
       </Card>
