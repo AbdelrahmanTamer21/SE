edd35340d49b99610ab94eb77e41ec1a93770fc7
Abdelrahman Tamer <abdelrahmantamer2005@yahoo.com>
2024-05-12 14:58:26 +0300

    Finished admin delete donor and org

--- donation-system/src/Pages/Admin/OrganizationList.js
+++ donation-system/src/Pages/Admin/OrganizationList.js
@@ -5,6 +5,7 @@ import Highlighter from 'react-highlight-words';
 import { useNavigate } from 'react-router-dom';
 import { Container } from "react-bootstrap";
 import OrganizationData from "../OrganizationData";
+import LoginData from "../LoginData";
 
 
 function OrganizationList() {
@@ -18,6 +19,7 @@ function OrganizationList() {
         navigate(`/Admin/OrganizationInfo/${org_id}`);
     }
 
+    const [data, setData] = useState(OrganizationData);
     const [searchText, setSearchText] = useState('');
     const [searchedColumn, setSearchedColumn] = useState('');
     const searchInput = useRef(null);
@@ -124,6 +126,21 @@ function OrganizationList() {
                 text
             ),
     });
+
+    const handleDelete = (id) => {
+        OrganizationData.forEach((item, index) => {
+            if (item.org_id === id) {
+                OrganizationData.splice(index, 1)
+            }
+        });
+        LoginData.forEach((item, index) => {
+            if (item.org_id === id) {
+                LoginData.splice(index, 1)
+            }
+        });
+        setData(data.filter((org) => org.org_id !== id));
+    }
+
     const columns = [
         {
             title: '#',
@@ -131,40 +148,69 @@ function OrganizationList() {
             width: '10%',
             render: (text, record, index) => index + 1,
         },
-    {
-        title: 'Name',
+        {
+            title: 'Name',
             dataIndex: 'organizationName',
-                key: 'name',
-                    width: '30%',
+            key: 'name',
+            width: '20%',
             ...getColumnSearchProps('name'),
         },
-    {
-        title: 'Type',
+        {
+            title: 'Type',
             dataIndex: 'orgType',
-                key: 'type',
-                    width: '30%',
+            key: 'type',
+            width: '15%',
             ...getColumnSearchProps('type'),
         },
-    {
-        title: 'Email',
+        {
+            title: 'Email',
             dataIndex: 'organizationEmail',
-                key: 'email',
+            key: 'email',
             ...getColumnSearchProps('email'),
             sorter: (a, b) => a.email.length - b.email.length,
-                sortDirections: ['descend', 'ascend'],
+            sortDirections: ['descend', 'ascend'],
+        },
+        {
+            title: 'Actions',
+            dataIndex: '',
+            key: 'x',
+            render: (_, record) => {
+                return (
+                    <Space size="middle">
+                        <Button onClick={() => handleDelete(record.org_id)} className="me-2" style={
+                            {
+                                backgroundColor: '#da0808',
+                                color: 'white',
+                            }
+                        }>
+                            Delete</Button>
+                        <Button onClick={() => handleRowClick(record.org_id)} className="me-2">Details</Button>
+                    </Space>
+                )
+            }
         }
     ];
+
+    const rowSelection = {
+        onChange: (selectedRowKeys, selectedRows) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        },
+        onSelect: (record, selected, selectedRows) => {
+            console.log(record, selected, selectedRows);
+        },
+        onSelectAll: (selected, selectedRows, changeRows) => {
+            console.log(selected, selectedRows, changeRows);
+        },
+    };
     return (
         <Container className="pt-3">
             <h1>Organization List</h1>
             <Table className="mt-4"
                 columns={columns}
-                dataSource={updatedOrganizationData}
-                onRow={(record, rowIndex) => {
-                    return {
-                        onClick: () => handleRowClick(record.org_id), // click row
-                    };
+                rowSelection={{
+                    ...rowSelection
                 }}
+                dataSource={data}
             />
         </Container>
     );
